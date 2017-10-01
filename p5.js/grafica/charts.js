4e72834d89fa07170f429564f8471958012a9940
Villem Alango <villem.alango@gmail.com>
2017-10-01 15:44:15 +0300

    nicer yRange(), better x-zoom

--- p5.js/grafica/charts.js
+++ p5.js/grafica/charts.js
@@ -3,7 +3,7 @@
 var SAMPLES = 200;              // Number of data samples.
 var RANGE   = 100;              // Slider values range.
 //  I kept this just for comparision;
-//  In real applications we'd prefer CSS styling instead.
+//  in real applications, we'd prefer CSS styling instead.
 var MARGINS = [50, 50, 40, 20]; // bottom, left, top, right
 
 //**************  Utility functions specific to this demo.
@@ -20,7 +20,7 @@ function sinPoints (x0, count, periods) {
 // Change x-magnification.
 function driver1 (data, ctrlValue) {
   var res  = {v0: 0, v1: 0, points: []};
-  var last = Math.floor(SAMPLES * (1 - ctrlValue / (2 * RANGE)));
+  var last = Math.floor(SAMPLES * (1 - ctrlValue / (1.05 * RANGE)));
 
   for (var i = 0; i < last; ++i) {
     res.points.push(new GPoint(res.v1 = data[i][0], data[i][1]));
@@ -38,20 +38,16 @@ function driver2 (data, ctrlValue) {
   return res;
 }
 
-// Fin d min/max values - just out of politeness here ;)
+// Find min/max values - just out of politeness here ;)
 function yRange (data) {
-  var min, max;
-  data.forEach(function (d) {
-    if (min === undefined) {
-      min = max = d[1];
-    } else if (d[1] < min) {
-      min = d[1];
-    } else if (d[1] > max) max = d[1];
-  })
-  return [Math.round(min), Math.round(max)];
+  var values = data.map(function (d) {
+    return d[1];  // This would look much nicer in ES5...
+  });
+  return [Math.round(Math.min.apply(null, values)),
+    Math.round(Math.max.apply(null, values))];
 }
 
-//**************  Actual Graphics housekeeping is below here.
+//**************  Actual Graphics Housekeeping is below here.
 
 // Factory pattern makes the code reusable.
 function factory (domId, title, data, driver) {
@@ -70,12 +66,12 @@ function factory (domId, title, data, driver) {
 
   // Actual definitions for P5 constructor.
   function graphics (p5instance) {
-    var plot;
+    var plot;   // grafica.js instance.
 
     p5instance.setup = function () {  // Lifecycle hook.
-      var gId    = domId + '-graphics';
-      var el     = document.getElementById(gId);
-      var w      = el.clientWidth, h = el.clientHeight || Math.floor(w * 2 / 5);
+      var gId = domId + '-graphics';
+      var el  = document.getElementById(gId);
+      var w   = el.clientWidth, h = el.clientHeight || Math.floor(w * 2 / 5);
 
       p5instance.createCanvas(w, h).parent(gId);
       renderSlider('#' + domId + '-slider');
@@ -95,9 +91,9 @@ function factory (domId, title, data, driver) {
 
       plot.setPoints(ctl.points);
       plot.setXLim(ctl.v0, ctl.v1);
-      // The following code could be replaced by just `plot.defaultDraw()`,
+      // The next 10 code lines could be replaced by just `plot.defaultDraw()`,
       // but this would result points being plotted too...
-      // Todo: figure out how to control point style.
+      // Todo: figure out, how to control points rendering.
       plot.beginDraw();
       plot.drawBackground();
       plot.drawBox();
@@ -116,4 +112,3 @@ function factory (domId, title, data, driver) {
 
 factory('section-1', 'sin(x)', sinPoints(0, SAMPLES, 4), driver1);
 factory('section-2', 'cos(x)', sinPoints(Math.PI / 2, SAMPLES, 4), driver2);
-
